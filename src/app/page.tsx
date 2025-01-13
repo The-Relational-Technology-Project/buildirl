0b35e94fe12ca005df75f6e93cffc2aaa0abbc2f
Michael Li <michael20051994@gmail.com>
2025-01-13 14:46:41 -0800

    remove example page references to example apis; those are not wired in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import styles from "./index.module.css";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <main className={styles.main}>
@@ -40,13 +35,6 @@ export default async function Home() {
               </div>
             </Link>
           </div>
-          <div className={styles.showcaseContainer}>
-            <p className={styles.showcaseText}>
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
         </div>
       </main>
     </HydrateClient>
