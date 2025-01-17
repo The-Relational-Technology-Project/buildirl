5f1347359dfb275dd7ea6c3a598ab38e87013196
Michael Li <michael20051994@gmail.com>
2025-01-16 17:06:29 -0800

    add a handshake endpoint for pkce auth flow

---
+++ src/app/api/auth/confirm.ts
@@ -0,0 +1,41 @@
+import { type EmailOtpType } from "@supabase/supabase-js";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createApiClient } from "~/utils/supabase/auth/client";
+import { rootLogger } from "~/logger";
+
+const logger = rootLogger.child({ module: "authHandler" });
+
+function stringOrFirstString(item: string | string[] | undefined) {
+  return Array.isArray(item) ? item[0] : item;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.status(405).appendHeader("Allow", "GET").end();
+    return;
+  }
+
+  const queryParams = req.query;
+  const token_hash = stringOrFirstString(queryParams.token_hash);
+  const type = stringOrFirstString(queryParams.type);
+
+  let next = "/";
+
+  if (token_hash && type) {
+    const supabase = createApiClient(req, res);
+    const { error } = await supabase.auth.verifyOtp({
+      type: type as EmailOtpType,
+      token_hash
+    });
+    if (error) {
+      logger.error(error);
+    } else {
+      next = stringOrFirstString(queryParams.next) ?? "/";
+    }
+  }
+
+  res.redirect(next);
+}
