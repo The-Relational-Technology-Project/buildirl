c2e4a00b796f6f98a148edad0e5266bd4f1b63f8
Michael Li <michael20051994@gmail.com>
2025-01-16 21:53:28 -0800

    setup chakra-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import {GeistSans} from "geist/font/sans";
+import {type Metadata} from "next";
 
-import { TRPCReactProvider } from "~/trpc/react";
+import {TRPCReactProvider} from "~/trpc/react";
+import {ChakraProvider} from "@chakra-ui/react";
+import {system} from "~/styles/theme";
 
 export const metadata: Metadata = {
-  title: "Build IRL",
-  description: "Supercharging builders of IRL communities",
-  icons: [{ rel: "icon", url: "/favicon.ico" }]
+    title: "Build IRL",
+    description: "Supercharging builders of IRL communities",
+    icons: [{rel: "icon", url: "/favicon.ico"}]
 };
 
 export default function RootLayout({
-  children
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body className={GeistSans.className}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
-  );
+                                       children
+                                   }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+        <body className={GeistSans.className}>
+        <TRPCReactProvider>
+            <ChakraProvider value={system}>
+                {children}
+            </ChakraProvider>
+        </TRPCReactProvider>
+        </body>
+        </html>
+    );
 }
