f928ad095d98ef6c71ea9c932b720f82e754e125
Michael Li <michael20051994@gmail.com>
2025-01-16 21:38:03 -0800

    wire up service to router

--- src/server/api/routers/main.ts
+++ src/server/api/routers/main.ts
@@ -1,13 +1,119 @@
 import { z } from "zod";
 
 import { createTRPCRouter, securedProcedure } from "~/server/api/trpc";
+import {
+    CreateClubInputSchema, CreateMembershipTierInputSchema,
+    CreateUserInputSchema, SubmitMembershipApplicationInputSchema, UpdateClubApplicationQuestionsInputSchema,
+    UpdateClubInputSchema, UpdateMembershipTierInputSchema,
+    UpdateUserInputSchema
+} from "~/server/service/types";
 
 export const mainRouter = createTRPCRouter({
-  hello: securedProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`
-      };
+  user: securedProcedure.query(({ ctx }) => {
+    return ctx.service.getUser(ctx.user.userId);
+  }),
+
+  userOwnedClubs: securedProcedure.query(({ ctx }) => {
+    return ctx.service.getUserOwnedClubs(ctx.user.userId);
+  }),
+
+  userMemberships: securedProcedure.query(({ ctx }) => {
+    return ctx.service.getUserMemberships(ctx.user.userId);
+  }),
+
+  clubByPublicId: securedProcedure
+    .input(z.object({publicId: z.string()}))
+    .query(({ ctx, input }) => {
+      return ctx.service.getClubByPublicId(input.publicId);
+    }),
+
+  activeMembershipsForClub: securedProcedure
+    .input(z.object({clubId: z.number()}))
+    .query(({ ctx, input }) => {
+      return ctx.service.getActiveMembershipsForClub(input.clubId);
+    }),
+
+  membershipApplicationsForClub: securedProcedure
+    .input(z.number())
+    .query(({ ctx, input }) => {
+      return ctx.service.getMembershipApplicationsForClub(input);
+    }),
+
+  clubStatistics: securedProcedure
+    .input(z.number())
+    .query(({ ctx, input }) => {
+      return ctx.service.getClubStatistics(input);
+    }),
+
+  club: securedProcedure
+    .input(z.number())
+    .query(({ ctx, input }) => {
+      return ctx.service.getClub(input);
+    }),
+
+  createUser: securedProcedure
+    .input(CreateUserInputSchema)
+    .mutation(({ ctx, input }) => {
+      return ctx.service.createUser(input, ctx.user.authUserId);
+    }),
+
+  updateUser: securedProcedure
+    .input(z.object({ id: z.number(), input: UpdateUserInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.updateUser(input.id, input.input);
+    }),
+
+  createClub: securedProcedure
+    .input(CreateClubInputSchema)
+    .mutation(({ ctx, input }) => {
+      return ctx.service.createClub(input, ctx.user.userId);
+    }),
+
+  updateClub: securedProcedure
+    .input(z.object({ id: z.number(), input: UpdateClubInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.updateClub(input.id, input.input);
+    }),
+
+  updateClubApplicationQuestions: securedProcedure
+    .input(z.object({ clubId: z.number(), input: UpdateClubApplicationQuestionsInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.updateClubApplicationQuestions(input.clubId, input.input);
+    }),
+
+  createMembershipTier: securedProcedure
+    .input(z.object({ clubId: z.number(), input: CreateMembershipTierInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.createMembershipTier(input.clubId, input.input);
+    }),
+
+  updateMembershipTier: securedProcedure
+    .input(z.object({ id: z.number(), input: UpdateMembershipTierInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.updateMembershipTier(input.id, input.input);
+    }),
+
+  submitMembershipApplication: securedProcedure
+    .input(z.object({ membershipTierId: z.number(), input: SubmitMembershipApplicationInputSchema }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.submitMembershipApplication(input.membershipTierId, input.input, ctx.user.userId);
+    }),
+
+  approveMembershipApplication: securedProcedure
+    .input(z.object({ membershipId: z.bigint() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.approveMembershipApplication(input.membershipId);
+    }),
+
+  declineMembershipApplication: securedProcedure
+    .input(z.object({ membershipId: z.bigint() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.declineMembershipApplication(input.membershipId);
+    }),
+
+  deactivateMembership: securedProcedure
+    .input(z.object({ membershipId: z.bigint() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.service.deactivateMembership(input.membershipId);
     })
 });
