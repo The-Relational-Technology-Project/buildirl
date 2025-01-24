ce36b074e2f58406554b7c7d83cc9a017cdb905b
Michael Li <michael20051994@gmail.com>
2025-01-23 23:11:32 -0800

    update club modal

--- src/client/components/OverviewPanel.tsx
+++ src/client/components/OverviewPanel.tsx
@@ -13,13 +13,101 @@ import { safeValidateSchema } from "~/utils/zod";
 import {
   Button,
   Group,
+  Image,
+  Modal,
+  Paper,
   Stack,
   Text,
   Textarea,
   TextInput,
+  ThemeIcon,
   Title
 } from "@mantine/core";
 import React from "react";
+import { notifications } from "@mantine/notifications";
+import { IconAlertTriangle, IconCheck } from "@tabler/icons-react";
+import { logger, notifyError } from "~/client/logger";
+import { useDisclosure } from "@mantine/hooks";
+
+type OverviewPanelProps = {
+  club: Club;
+};
+
+export function OverviewPanel({ club }: OverviewPanelProps) {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Paper radius="md" p="xl" withBorder mt={20}>
+        <Group justify={"flex-start"} align={"stretch"} gap={40}>
+          <Image
+            radius="md"
+            w={300}
+            h={300}
+            fallbackSrc={"/image-fallback.png"}
+          />
+          <Stack justify={"space-between"} style={{ flex: 1 }}>
+            <Stack gap={6}>
+              <Title order={4}>Club Details</Title>
+              <Title order={5} mt={2}>
+                Name
+              </Title>
+              <Text>{club.name}</Text>
+              <Title order={5}>Tagline</Title>
+              {club.tagLine === "" ? (
+                <Group gap={4}>
+                  <ThemeIcon color={"orange.5"} variant={"white"} size={"xs"}>
+                    <IconAlertTriangle />
+                  </ThemeIcon>
+                  <Text c={"orange.5"}>
+                    Please enter tagline and other basic information.
+                  </Text>
+                </Group>
+              ) : (
+                <Text>{club.tagLine}</Text>
+              )}
+            </Stack>
+            <Group grow>
+              <Button mt={"sm"} onClick={open}>
+                Edit Club Page
+              </Button>
+              <Button
+                mt={"sm"}
+                onClick={async () => {
+                  await copyToClipboard(club.publicId);
+                }}
+              >
+                Share
+              </Button>
+            </Group>
+          </Stack>
+        </Group>
+      </Paper>
+
+      <Modal opened={opened} onClose={close}>
+        <UpdateClubForm club={club} />
+      </Modal>
+    </>
+  );
+}
+
+async function copyToClipboard(clubPublicId: string): Promise<void> {
+  const url = window.location.origin + "/share/" + clubPublicId;
+
+  try {
+    await navigator.clipboard.writeText(url);
+    notifications.show({
+      title: "Link copied",
+      message: "Share link has been copied to clipboard",
+      color: "green",
+      icon: <IconCheck size="1.1rem" />,
+      autoClose: 3000
+    });
+  } catch (e) {
+    logger.error("error while copying to clipboard: " + e);
+    notifyError();
+  }
+}
 
 type UpdateClubFormProps = {
   club: Club;
@@ -76,15 +164,14 @@ function UpdateClubForm({ club }: UpdateClubFormProps) {
             name: name,
             tagLine: tagLine,
             description: description,
-            websiteURL: websiteURL,
-            instagramHandle: instagramHandle,
-            eventCalendarURL: eventCalendarURL
+            websiteURL: websiteURL === "" ? null : websiteURL,
+            instagramHandle: instagramHandle === "" ? null : instagramHandle,
+            eventCalendarURL: eventCalendarURL === "" ? null : eventCalendarURL
           });
         }
       )}
     >
-      <Stack>
-        <Title>Create club</Title>
+      <Stack p={"md"} gap={8}>
         <TextInput
           required
           placeholder="Club name"
@@ -93,7 +180,6 @@ function UpdateClubForm({ club }: UpdateClubFormProps) {
             form.setFieldValue("name", event.currentTarget.value)
           }
           error={form.errors.name}
-          size={"xl"}
         />
         <TextInput
           required
@@ -115,20 +201,24 @@ function UpdateClubForm({ club }: UpdateClubFormProps) {
           autosize
           minRows={3}
         />
-        <Title order={4}>Links</Title>
+        <Title order={6} mt={6}>
+          Links
+        </Title>
         <TextInput
-          placeholder="Website link (optional)"
-          value={form.values.websiteURL ?? undefined}
+          placeholder="Website link"
+          value={form.values.websiteURL ?? ""}
           onChange={(event) =>
             form.setFieldValue("websiteURL", event.currentTarget.value)
           }
           error={form.errors.websiteURL}
         />
         <Group gap={4}>
-          <Text c={"dimmed"}>instagram.com/</Text>
+          <Text c={"dimmed"} size={"sm"}>
+            instagram.com/
+          </Text>
           <TextInput
-            placeholder="tag (optional)"
-            value={form.values.instagramHandle ?? undefined}
+            placeholder="tag"
+            value={form.values.instagramHandle ?? ""}
             onChange={(event) =>
               form.setFieldValue("instagramHandle", event.currentTarget.value)
             }
@@ -136,16 +226,20 @@ function UpdateClubForm({ club }: UpdateClubFormProps) {
           />
         </Group>
         <TextInput
-          placeholder="Calendar link (optional)"
-          value={form.values.eventCalendarURL ?? undefined}
+          placeholder="Event calendar link (e.g., Luma)"
+          value={form.values.eventCalendarURL ?? ""}
           onChange={(event) =>
             form.setFieldValue("eventCalendarURL", event.currentTarget.value)
           }
           error={form.errors.eventCalendarURL}
         />
-        <Title order={4}>Choose a share link</Title>
+        <Title order={6} mt={6}>
+          Share link
+        </Title>
         <Group gap={4}>
-          <Text c={"dimmed"}>buildirl.com/share/</Text>
+          <Text c={"dimmed"} size={"sm"}>
+            buildirl.com/share/
+          </Text>
           <TextInput
             required
             placeholder="club-tag"
@@ -163,7 +257,7 @@ function UpdateClubForm({ club }: UpdateClubFormProps) {
           style={{ alignSelf: "center" }}
           disabled={!form.isValid() || createUser.isPending}
         >
-          Create
+          Save
         </Button>
       </Stack>
     </form>
