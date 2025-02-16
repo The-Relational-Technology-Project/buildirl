4477b58842316e3a50d54a175b33f2aff2fea2a9
Michael Li <michael20051994@gmail.com>
2025-02-16 15:53:40 -0800

    club display image gallery and upload component; work-in-progress

---
+++ src/app/(main)/playground/[clubId]/__components/ClubDisplayImageGallery.tsx
@@ -0,0 +1,25 @@
+import { Group, ScrollArea, Image } from "@mantine/core";
+import { Club } from "~/server/service/types";
+
+type ClubDisplayImageGalleryProps = {
+  club: Club;
+};
+
+export function ClubDisplayImageGallery({
+  club
+}: ClubDisplayImageGalleryProps) {
+  return (
+    <ScrollArea>
+      <Group>
+        {club?.displayImageUrls.map((url, index) => (
+          <Image
+            key={index}
+            src={url}
+            alt={`Club Image ${index}`}
+            width={150}
+          />
+        ))}
+      </Group>
+    </ScrollArea>
+  );
+}
---
+++ src/app/(main)/playground/[clubId]/__components/ClubDisplayImageUpload.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Group, Image, ActionIcon } from "@mantine/core";
+import { api } from "~/trpc/react";
+import { storageClient } from "~/client/utils/storageClient";
+import { Dropzone } from "@mantine/dropzone";
+import { Club } from "~/server/service/types";
+
+type ClubDisplayImageUploadProps = {
+  club: Club;
+};
+
+export function ClubDisplayImageUpload({ club }: ClubDisplayImageUploadProps) {
+  const utils = api.useUtils();
+  const [uploadedUrls, setUploadedUrls] = useState<string[]>([]);
+
+  const updateClubDisplayImageUrlsMutation =
+    api.main.updateClubDisplayImageUrls.useMutation({
+      onSuccess: () => {
+        utils.main.club.invalidate({ id: club.id });
+        utils.main.clubByPublicId.invalidate({ publicId: club.publicId });
+        utils.main.userOwnedClubs.invalidate();
+      }
+    });
+
+  const handleUpload = async (files: File[]) => {
+    const newUrls = await Promise.all(
+      files.map(async (file) => {
+        return await storageClient.uploadClubDisplayImage(club.id, file);
+      })
+    );
+
+    setUploadedUrls((prev) => [...prev, ...newUrls]);
+
+    await updateClubDisplayImageUrlsMutation.mutateAsync({
+      clubId: club.id,
+      input: { displayImageUrls: uploadedUrls.concat(newUrls) }
+    });
+  };
+
+  const handleDelete = async (url: string) => {
+    await storageClient.deleteClubDisplayImage(club.id, url);
+
+    const updatedUrls = uploadedUrls.filter((u) => u !== url);
+    setUploadedUrls(updatedUrls);
+
+    await updateClubDisplayImageUrlsMutation.mutateAsync({
+      clubId: club.id,
+      input: { displayImageUrls: updatedUrls }
+    });
+  };
+
+  return (
+    <Dropzone onDrop={handleUpload} maxFiles={5}>
+      <Group>
+        {uploadedUrls.map((url, index) => (
+          <div key={index}>
+            <Image src={url} alt={`Club Image ${index}`} width={100} />
+            <ActionIcon onClick={() => handleDelete(url)}>x</ActionIcon>
+          </div>
+        ))}
+      </Group>
+    </Dropzone>
+  );
+}
