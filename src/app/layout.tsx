5833ac9d6474d919bc1e5a98555d685b98d1d505
Michael Li <michael20051994@gmail.com>
2025-02-20 22:11:15 -0800

    disable pinch to zoom in mobile mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,10 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript defaultColorScheme={"light"} />
       </head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
       <body className={manrope.className}>
         <TRPCReactProvider>
           <MantineProvider theme={theme} defaultColorScheme={"light"}>
