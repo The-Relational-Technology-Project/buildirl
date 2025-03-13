c47228833c41542503b676428a03b6022122580d
Michael Li <michael20051994@gmail.com>
2025-03-13 14:38:50 -0700

    start stripe connect panel

--- src/app/(main)/settings/_components/StripeConnectPanel.tsx
+++ src/app/(main)/settings/_components/StripeConnectPanel.tsx
@@ -1,3 +1,25 @@
-export default function StripeConnectPanel() {
+import { Button, Stack, Text, Title } from "@mantine/core";
+import React from "react";
+
+function CreateStripeConnectAccount() {
+  return (
+    <Button w={200} mt={"lg"}>
+      Create Account
+    </Button>
+  );
+}
+
+function ManageStripeConnectAccount() {
   return <></>;
+}
+
+export default function StripeConnectPanel() {
+  return (
+    <Stack mt={"lg"} gap={4}>
+      <Title order={4}>Stripe Connect</Title>
+      <Text size={"md"}>
+        Use your Stripe Connect account to receive member contributions.
+      </Text>
+    </Stack>
+  );
 }
