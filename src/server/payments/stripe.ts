e4916bf5f46a18200569ef43636de83e16952fdf
Michael Li <michael20051994@gmail.com>
2025-03-14 11:17:20 -0700

    singleton pattern for loading stripe

--- src/server/payments/stripe.ts
+++ src/server/payments/stripe.ts
@@ -1,7 +1,15 @@
 import Stripe from "stripe";
 import { env } from "~/env";
 
-// TODO singleton pattern https://vercel.com/guides/getting-started-with-nextjs-typescript-stripe
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-02-24.acacia"
-});
+const createStripeClient = () =>
+  new Stripe(env.STRIPE_SECRET_KEY, {
+    apiVersion: "2025-02-24.acacia"
+  });
+
+const globalForStripe = globalThis as unknown as {
+  stripe: ReturnType<typeof createStripeClient> | undefined;
+};
+
+export const stripe = globalForStripe.stripe ?? createStripeClient();
+
+if (env.NODE_ENV !== "production") globalForStripe.stripe = stripe;
