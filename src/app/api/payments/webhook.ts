b8a24f8c8a4ba2ce996edb408a1d488eed260c22
Michael Li <michael20051994@gmail.com>
2025-03-16 18:41:40 -0700

    webhook handling for checkout.session.completed

--- src/app/api/payments/webhook.ts
+++ src/app/api/payments/webhook.ts
@@ -2,8 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { rootLogger } from "~/logger";
 import { env } from "~/env";
 import { stripe } from "~/server/payments/stripe/stripe";
-import { assertAsString } from "~/utils";
+import { assertAsString, stringify } from "~/utils";
 import Cors from "micro-cors";
+import { prisma } from "~/server/prisma";
+import Stripe from "stripe";
 
 const cors = Cors({
   allowMethods: ["POST", "HEAD"]
@@ -16,6 +18,42 @@ export const config = {
 };
 
 const logger = rootLogger.child({ module: "stripeWebhookHandler" });
+
+/**
+ * Idempotent update of membership with setupIntentId from stripe
+ *
+ * It is important this is idempotent because stripe can send webhook event
+ * multiple times
+ */
+async function updateMembershipWithStripeSetupIntentId(
+  membershipId: bigint,
+  setupIntentId: string
+): Promise<void> {
+  try {
+    await prisma.membership.update({
+      where: { id: membershipId },
+      data: { stripeSetupIntentId: setupIntentId }
+    });
+    logger.info(
+      `updated membership with id ${membershipId} with stripeSetupIntentId ${setupIntentId}`
+    );
+  } catch (e) {
+    logger.error(
+      e,
+      `failed to update membership with id ${membershipId} with stripeSetupIntentId ${setupIntentId}`
+    );
+    throw e;
+  }
+}
+
+function getSetupIntent(
+  setupIntent: string | Stripe.SetupIntent
+): Promise<Stripe.SetupIntent> {
+  if (typeof setupIntent === "string") {
+    return stripe.setupIntents.retrieve(setupIntent);
+  }
+  return Promise.resolve(setupIntent);
+}
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
@@ -31,7 +69,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   }
 
   try {
-    // verify and construct the event
     const event = stripe.webhooks.constructEvent(
       req.body,
       signature,
@@ -42,7 +79,36 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     // https://docs.stripe.com/billing/subscriptions/webhooks
     switch (event.type) {
       case "checkout.session.completed":
-        throw new Error("unimplemented");
+        const session = event.data.object as Stripe.Checkout.Session;
+
+        if (!session.setup_intent) {
+          // we cannot guarantee in future there are not other checkout sessions not
+          // created by our system
+          logger.warn(
+            `checkout session ${session.id} completed without a setup intent`
+          );
+          break;
+        }
+
+        const setupIntent = await getSetupIntent(session.setup_intent);
+
+        if (!setupIntent.metadata?.externalMembershipId) {
+          const errorMessage = `setup intent ${setupIntent.id} missing externalMembershipId`;
+          logger.error(errorMessage);
+          // do not throw here because we do not want Stripe to retry even if it is unexpected bad data
+          break;
+        }
+
+        const membershipId = BigInt(setupIntent.metadata.externalMembershipId);
+        await updateMembershipWithStripeSetupIntentId(
+          membershipId,
+          setupIntent.id
+        );
+
+        logger.info(
+          `successfully processed checkout.session.completed event for session ${session.id}`
+        );
+        break;
       default:
         // TODO for failed cases (e.g. invoice.payment_failed), should we at
         //  least alert as error?
@@ -51,8 +117,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     res.status(200).json({ received: true });
   } catch (e) {
-    logger.error(e, `webhook error: ${e}`);
-    res.status(400).json({ error: `webhook error: ${e}` });
+    logger.error(e, `webhook error for req ${stringify(req.body)}`);
+    // errors here will be retried by Stripe
+    // TODO we can observe if there are classes and distinguish errors that are not retryable
+    res.status(400).json({ error: `webhook error: ${stringify(e)}` });
   }
 }
 
