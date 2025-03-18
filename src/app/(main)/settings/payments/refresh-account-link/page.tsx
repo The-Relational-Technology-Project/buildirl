4ee1277c4b7a522765f9096c0a43fd2e10a56821
Michael Li <michael20051994@gmail.com>
2025-03-18 16:52:52 -0700

    implement refresh account link page

--- src/app/(main)/settings/payments/refresh-account-link/page.tsx
+++ src/app/(main)/settings/payments/refresh-account-link/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { useEffect } from "react";
+import { Alert, Loader, Stack, Text, Title } from "@mantine/core";
+import { api } from "~/trpc/react";
+import { logger } from "~/client/logger";
+import AbsoluteCenter from "~/client/components/AbsoluteCenter";
+
 export default function RefreshAccountLink() {
-  return <></>;
+  const createAccountLink = api.payments.createAccountLink.useMutation({
+    onSuccess: (data) => {
+      window.location.href = data.redirectUrl;
+    },
+    onError: (e) => {
+      logger.error(e, "failed to refresh account link");
+    }
+  });
+
+  useEffect(() => {
+    createAccountLink.mutate({
+      origin: window.location.origin
+    });
+  }, []);
+
+  return (
+    <AbsoluteCenter adjustForHeader={true}>
+      <Stack align="center" gap={"xl"} justify="center">
+        <Title order={3} fw={500}>
+          Refreshing Your Account Link
+        </Title>
+
+        {createAccountLink.isPending && (
+          <Stack align="center" gap="md">
+            <Text>Creating a new account link...</Text>
+            <Loader size="lg" color={"black"} />
+          </Stack>
+        )}
+
+        {createAccountLink.isError && (
+          <Alert color="red" title="Error" maw={500}>
+            An unexpected error occurred while refreshing your account link.
+            Please try again or contact support if this issue persists.
+          </Alert>
+        )}
+      </Stack>
+    </AbsoluteCenter>
+  );
 }
