030d36f5ead088c950d2b3d60cbbcbb478cceed5
Michael Li <michael20051994@gmail.com>
2025-04-01 08:10:27 -0700

    add retries to stripe client; operations should be idempotent and we do want some resilience on calls

--- src/server/payments/stripe/stripe.ts
+++ src/server/payments/stripe/stripe.ts
@@ -3,7 +3,8 @@ import { env } from "~/env";
 
 const createStripeClient = () =>
   new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-02-24.acacia"
+    apiVersion: "2025-02-24.acacia",
+    maxNetworkRetries: 3
   });
 
 const globalForStripe = globalThis as unknown as {
