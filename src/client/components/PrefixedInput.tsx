a4ca303c24af806f1d304a21e9e03561ec9363e0
Michael Li <michael20051994@gmail.com>
2025-04-28 17:15:55 -0700

    the error input was shifting the textinput out of alignment; use a custom error message instead and add a comment explaining

--- src/client/components/PrefixedInput.tsx
+++ src/client/components/PrefixedInput.tsx
@@ -5,7 +5,8 @@ import {
   TextInput,
   TextInputProps,
   useMantineTheme,
-  useMantineColorScheme
+  useMantineColorScheme,
+  Box
 } from "@mantine/core";
 
 /**
@@ -17,6 +18,7 @@ type PrefixedInputProps = {
 
 export default function PrefixedInput({
   prefix,
+  error,
   ...props
 }: PrefixedInputProps) {
   const theme = useMantineTheme();
@@ -25,32 +27,45 @@ export default function PrefixedInput({
 
   const borderColor = isDark ? theme.colors.dark[1] : theme.colors.dark[9];
   const bgColor = isDark ? theme.colors.dark[6] : theme.colors.gray[0];
-
+  
   return (
-    <Group gap={0} wrap="nowrap">
-      <Text
-        size="sm"
-        style={{
-          background: bgColor,
-          padding: "8px 8px",
-          borderRadius: "4px 0 0 4px",
-          border: `1px solid ${borderColor}`,
-          borderRight: "none"
-        }}
-      >
-        {prefix}
-      </Text>
-      <TextInput
-        w="100%"
-        styles={{
-          input: {
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-            border: `1px solid ${borderColor}`
-          }
-        }}
-        {...props}
-      />
-    </Group>
+    <Box>
+      <Group gap={0} wrap="nowrap">
+        <Text
+          size="sm"
+          style={{
+            background: bgColor,
+            padding: "8px 8px",
+            borderRadius: "4px 0 0 4px",
+            border: `1px solid ${borderColor}`,
+            borderRight: "none"
+          }}
+        >
+          {prefix}
+        </Text>
+        <TextInput
+          w="100%"
+          styles={{
+            input: {
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              border: `1px solid ${borderColor}`
+            },
+            error: {
+              marginTop: '5px'
+            }
+          }}
+          // use the error message instead as this error message will 
+          // mess up the layout
+          error={null}
+          {...props}
+        />
+      </Group>
+      {error && (
+        <Text c="red" size="xs" mt={5}>
+          {error}
+        </Text>
+      )}
+    </Box>
   );
 }
